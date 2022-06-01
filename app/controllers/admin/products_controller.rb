a4d731ab3d68efff6d56770e5dc9775fcfec7306
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-06-01 14:02:22 +0000

    add user authentication, no ENV capability yett

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,9 @@
 class Admin::ProductsController < ApplicationController
+  name = ENV['USERNAME']
+  password = ENV['PASSWORD']
+
+  http_basic_authenticate_with name: 'Jungle', password: 'book'
+
 
   def index
     @products = Product.order(id: :desc).all
